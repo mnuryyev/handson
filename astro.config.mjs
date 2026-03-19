2cdaba1ee0c604b0378baf19d1ea87bcdcdf9dc9
mnuryyev <151946991+mnuryyev@users.noreply.github.com>
2026-03-19 05:43:41 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Укажи адрес своего сайта на GitHub Pages
   site: 'https://mnuryyev.github.io',
-  // Укажи название репозитория в начале и конце со слешами
   base: '/handson/', 
-  // ... твои остальные настройки (интеграции и т.д.)
 });
