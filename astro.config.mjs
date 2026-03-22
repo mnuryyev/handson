d3b4dc6ecd2d1716d57d73aac2ad618ccb8a10e5
mnuryyev <151946991+mnuryyev@users.noreply.github.com>
2026-03-22 13:08:20 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap'; // 1. Добавили импорт плагина
 
 export default defineConfig({
   site: 'https://mnuryyev.github.io',
-  base: '/handson/', 
+  base: '/handson/',
+  integrations: [
+    sitemap() // 2. Включили генерацию карты сайта
+  ],
 });
